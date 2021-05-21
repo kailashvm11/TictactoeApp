2d64dd4e469b7440932f64a408ff4986e3755dc4
Rajesh <kailashvm@yahoo.com>
2021-05-21 03:42:05 +0200

    refactor: make all the test cases as part of nested class

--- src/test/java/com/tictactoe/TictactoeAppTest.java
+++ src/test/java/com/tictactoe/TictactoeAppTest.java
@@ -1,11 +1,10 @@
 package com.tictactoe;
+
 import ch.qos.logback.classic.Logger;
 import ch.qos.logback.classic.spi.ILoggingEvent;
 import ch.qos.logback.core.read.ListAppender;
 import org.assertj.core.api.Assertions;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 import org.slf4j.LoggerFactory;
 
 import java.util.List;
@@ -16,91 +15,94 @@ import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
 class TictactoeAppTest {
+    @Nested
+    @DisplayName("Test cases without conflicting input")
+    class InputMockTestWithoutConflict {
+        Logger gameLogger = (Logger) LoggerFactory.getLogger(TictactoeGame.class);
+        TictactoeGame game;
+        ListAppender<ILoggingEvent> listAppender;
 
-    Logger gameLogger = (Logger) LoggerFactory.getLogger(TictactoeGame.class);
-    TictactoeGame game;
-    ListAppender<ILoggingEvent> listAppender;
+        @BeforeEach
+        void setUp() {
 
-    @BeforeEach
-    void setUp() {
+            listAppender = new ListAppender<>();
+            listAppender.start();
+            gameLogger.addAppender(listAppender);
+            InputHandler mockInputHandler = mock(InputHandler.class);
+            when(mockInputHandler.readInput('X'))
+                    .thenReturn("2,3")
+                    .thenReturn("1,1")
+                    .thenReturn("2,2")
+                    .thenReturn("3,3");
+            when(mockInputHandler.readInput('O'))
+                    .thenReturn("2,1")
+                    .thenReturn("3,1")
+                    .thenReturn("3,2");
+            game = new TictactoeGame(mockInputHandler);
+        }
 
-        listAppender = new ListAppender<>();
-        listAppender.start();
-        gameLogger.addAppender(listAppender);
-        InputHandler mockInputHandler = mock(InputHandler.class);
-        when(mockInputHandler.readInput('X'))
-                .thenReturn("2,3")
-                .thenReturn("1,1")
-                .thenReturn("2,2")
-                .thenReturn("3,3");
-        when(mockInputHandler.readInput('O'))
-                .thenReturn("2,1")
-                .thenReturn("3,1")
-                .thenReturn("3,2");
-        game = new TictactoeGame(mockInputHandler);
-    }
+        @Test
+        public void shouldAnswerWithTrue() {
+            TictactoeApp tictactoeApp = new TictactoeApp();
+            assertNotNull(tictactoeApp);
+        }
 
-    @Test
-    public void shouldAnswerWithTrue() {
-        TictactoeApp tictactoeApp = new TictactoeApp();
-        assertNotNull( tictactoeApp );
-    }
+        @Test
+        void shouldDisplayStartMessage() {
+            game.start();
+            List<ILoggingEvent> logsList = listAppender.list;
+            assertEquals("Starting a new Tic-tac-toe Game", logsList.get(0)
+                    .getMessage());
 
-    @Test
-    void shouldDisplayStartMessage() {
-        game.start();
-        List<ILoggingEvent> logsList = listAppender.list;
-        assertEquals("Starting a new Tic-tac-toe Game", logsList.get(0)
-                .getMessage());
+        }
 
-    }
+        @Test
+        void shouldDisplayEmptyGrid() {
+            game.start();
+            List<ILoggingEvent> logsList = listAppender.list;
+            assertEquals("---------", logsList.get(1).getMessage());
+            assertEquals("|       |", logsList.get(2).getMessage());
+            assertEquals("|       |", logsList.get(3).getMessage());
+            assertEquals("|       |", logsList.get(4).getMessage());
+            assertEquals("---------", logsList.get(5).getMessage());
+        }
 
-    @Test
-    void shouldDisplayEmptyGrid() {
-        game.start();
-        List<ILoggingEvent> logsList = listAppender.list;
-        assertEquals("---------", logsList.get(1).getMessage());
-        assertEquals("|       |", logsList.get(2).getMessage());
-        assertEquals("|       |", logsList.get(3).getMessage());
-        assertEquals("|       |", logsList.get(4).getMessage());
-        assertEquals("---------", logsList.get(5).getMessage());
-    }
+        @Test
+        void shouldUpdateGrid() {
+            game.start();
+            char[][] grid = game.getGrid();
+            assertEquals('X', grid[1][2]);
+        }
 
-    @Test
-    void shouldUpdateGrid() {
-        game.start();
-        char[][] grid = game.getGrid();
-        assertEquals('X', grid[1][2]);
-    }
+        @Test
+        void shouldDisplayGridAfterUpdate() {
+            game.start();
+            List<ILoggingEvent> logsList = listAppender.list;
+            Assertions.assertThat(listAppender.list)
+                    .extracting(ILoggingEvent::getFormattedMessage)
+                    .contains("|     X |");
+        }
 
-    @Test
-    void shouldDisplayGridAfterUpdate() {
-        game.start();
-        List<ILoggingEvent> logsList = listAppender.list;
-        Assertions.assertThat(listAppender.list)
-                .extracting(ILoggingEvent::getFormattedMessage)
-                .contains("|     X |");
-    }
+        @Test
+        void shouldUpdateGridAfterO() {
+            game.start();
+            char[][] grid = game.getGrid();
+            assertEquals('O', grid[1][0]);
+        }
 
-    @Test
-    void shouldUpdateGridAfterO() {
-        game.start();
-        char[][] grid = game.getGrid();
-        assertEquals('O', grid[1][0]);
-    }
+        @Test
+        void shouldDisplayGridAfterOUpdate() {
+            game.start();
+            List<ILoggingEvent> logsList = listAppender.list;
+            Assertions.assertThat(listAppender.list)
+                    .extracting(ILoggingEvent::getFormattedMessage)
+                    .contains("| O   X |");
+        }
 
-    @Test
-    void shouldDisplayGridAfterOUpdate() {
-        game.start();
-        List<ILoggingEvent> logsList = listAppender.list;
-        Assertions.assertThat(listAppender.list)
-                .extracting(ILoggingEvent::getFormattedMessage)
-                .contains("| O   X |");
-    }
-
-    @AfterEach
-    void tearDown() {
-        listAppender.stop();
-        listAppender.list.clear();
+        @AfterEach
+        void tearDown() {
+            listAppender.stop();
+            listAppender.list.clear();
+        }
     }
 }
